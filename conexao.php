c2fef8308d1e1d049d22f932f4a3ef9a93969f44
Matheus Pereira <63103696+bigodinho16@users.noreply.github.com>
2020-05-13 20:27:25 -0300

    Delete conexao.php

--- conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servidor = "localhost";
-$usuario = "matheus";
-$senha = "pereira";
-$dbname = "siteapostas";
-
-//Criar a conexao
-$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
